4c500dadbc2a36ee1fe4bc35d4344e77d9463377
Andrei Lazarchik <desto@mail.ru>
2020-07-21 22:30:38 +0300

    Test commitment

--- lesson_02/task07.java
+++ lesson_02/task07.java
@@ -7,9 +7,9 @@ public class task07 {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
 
-        if(num == 1 || (num%10 == 1 && num != 11)) System.out.println(num + " программист");
-         else if((1<num && num<5) || (num%10 == 2 && num != 12) || (num%10 == 3 && num != 13) ||
-                (num%10 == 4 && num != 14)) System.out.println(num + " программиста");
+        if(num == 1 || (num%10 == 1 && num != 11 && num%100 != 11) ) System.out.println(num + " программист");
+         else if((1<num && num<5) || (num%10 == 2 && num != 12 && num%100 != 12) || (num%10 == 3 && num != 13 && num%100 != 13) ||
+                (num%10 == 4 && num != 14 && num%100 != 14)) System.out.println(num + " программиста");
             else if(num>=0) System.out.println(num + " программистов");
     }
 }
